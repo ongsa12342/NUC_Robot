7de47429a27d66b5903221145e223284185e1f9d
Papinwit Rattanasiri <61608951+ongsa12342@users.noreply.github.com>
2024-12-15 15:45:35 +0700

    Update initial.launch.py

--- src/navigate_NUC/launch/initial.launch.py
+++ src/navigate_NUC/launch/initial.launch.py
@@ -1,9 +1,9 @@
 import os
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
 from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -35,9 +35,62 @@ def generate_launch_description():
         output='screen'
     )
 
+    ekf_launch_path = os.path.join(
+        get_package_share_directory('robot_localization'), 'launch', 'ekf.launch.py'
+    )
+    point_to_laser_launch_path = os.path.join(
+        get_package_share_directory('convert_odom'), 'launch', 'custom_pointcloud_to_laserscan_launch.py'
+    )
+
+    Filter_LP_node = Node(
+        package='convert_odom',
+        executable='accel_filter.py',
+        name='accel_filter',
+        output='screen',
+    )
+
+    convert_odom_type_node = Node(
+        package='convert_odom',
+        executable='convert_array_odom.py',
+        name='convert_array_odom',
+        output='screen',
+    )
+
+    time_sync_node = Node(
+        package='convert_odom',
+        executable='time_sync.py',
+        name='Synctime',
+    )
+
+    tf = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_transform_publisher',
+        arguments=[
+            '0', '0', '0',  # x, y, z
+            '0', '0', '0', '1',  # Quaternion qx, qy, qz, qw
+            'map',  # Parent frame
+            'odom'  # Child frame
+        ]
+    )
+
+    point_to_laser_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(point_to_laser_launch_path)
+    )
+
+    ekf_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(ekf_launch_path)
+    )
+
     ld.add_action(unitree_lidar_action)
     ld.add_action(uros_action)
     ld.add_action(teleop_joy_action)
     ld.add_action(teleop_twist_joy_action)
+    ld.add_action(ekf_launch)
+    ld.add_action(Filter_LP_node)
+    ld.add_action(convert_odom_type_node)
+    ld.add_action(time_sync_node)
+    ld.add_action(tf)
+    ld.add_action(point_to_laser_launch)
 
     return ld
